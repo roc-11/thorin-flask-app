6adfe38fc8086b9560b9d7157faf8b7a9016b3ab
roc-11 <>
2024-01-07 19:51:20 +0000

    Template inheritance with Flask

--- run.py
+++ run.py
@@ -13,9 +13,15 @@ def index():
 def about():
     return render_template("about.html")
 
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
 
 
 if __name__ == "__main__":
